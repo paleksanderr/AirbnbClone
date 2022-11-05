998d888e68fe8aa6c6f1667c036bcd7adb861c4f
Paweł Aleksander <pawel.aleksander@gmail.com>
2022-11-05 12:13:34 +0100

    add home screen

--- Airbnb/App.js
+++ Airbnb/App.js
@@ -8,26 +8,17 @@
 
 import React from 'react';
 
-import {SafeAreaView, StatusBar, useColorScheme} from 'react-native';
+import {SafeAreaView} from 'react-native';
 
-import {Colors} from 'react-native/Libraries/NewAppScreen';
+import HomeScreen from './src/screens/Home';
 
 /* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
  * LTI update could not be added via codemod */
 
 const App: () => React$Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
+    <SafeAreaView>
+      <HomeScreen />
     </SafeAreaView>
   );
 };
